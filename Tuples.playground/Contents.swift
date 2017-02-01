445af4605912c26707d829bed93abc5a7f3f8845
bgb0923 <bgb0923@gmail.com>
2017-02-01 13:10:19 -0500

    Followed along with lesson

--- Tuples.playground/Contents.swift
+++ Tuples.playground/Contents.swift
@@ -1,23 +1,6 @@
-func downloadImage(atURL url: String) -> (Bool, String) {
-    // TODO: Download image
-    // if no error, return true
-    // otherwise, get error string and return false
-    let error = "Image no longer exists"
-    return (false, error)
-}
-
-let result = downloadImage(atURL: "http://example.com/image.png")
-result.0
-result.1
-print("Success? \(result.0)")
-print("Message: \(result.1)")
-
-let person = (name: "Jim", age: 30)
-person.age
-person.name
+let pl: (Int, Int) = (1,2)
+let x: (Bool, String) = (false, "Image no longer exists at URL")
+let person = (name: "jim", age: 30)
 print("\(person.name)'s age is \(person.age)")
-
 var person2 = (name: "Jim", age: 30)
-print("\(person2.name)'s age is \(person2.age)")
-person2.age = 32
 print("\(person2.name)'s age is \(person2.age)")
